0fa7ab82a448519e9d6e1500ac2d061bbcab0765
Max Penet <max.penet@exoscale.ch>
2020-05-15 16:33:12 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/coax "0.1.4-SNAPSHOT"
+(defproject exoscale/coax "1.0.0-alpha1-SNAPSHOT"
   :description "exo spec-coerce fork"
   :url "https://github.com/exoscale/coax"
   :dependencies [[org.clojure/clojure "1.10.1"]]
