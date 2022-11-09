cdd988aeabc9abf7015879360577f67455639705
Anthony Galea <anthony.galea@gmail.com>
2022-11-09 10:17:10 +0100

    test cljs

--- test/exoscale/coax/cljs_test_runner.cljs
+++
@@ -1,6 +0,0 @@
-(ns exoscale.coax.cljs-test-runner
-  (:require [cljs.test :refer-macros [run-tests]] ; new
-            [exoscale.coax-test]))
-
-(enable-console-print!)
-(run-tests 'exoscale.coax-test)
